e867979a685e6926589ebed05f858eef8daaf86a
Daniil Zaytsev <kupernewfag@gmail.com>
2023-05-28 20:56:48 +0300

    added 2 tasks

---
+++ Ya.Practicum.SprintFour/APolynomialHashes.cs
@@ -0,0 +1,22 @@
+﻿namespace Ya.Practicum.SprintFour;
+
+/// <summary>
+/// Алле очень понравился алгоритм вычисления полиномиального хеша. Помогите ей написать функцию,
+/// вычисляющую хеш строки s. В данной задаче необходимо использовать
+/// в качестве значений отдельных символов их коды в таблице ASCII.
+/// </summary>
+public static class APolynomialHashes
+{
+    public static void Execute()
+    {
+        using var reader = new StreamReader(Console.OpenStandardInput());
+        var a = int.Parse(reader.ReadLine()!);
+        var m = int.Parse(reader.ReadLine()!);
+        var str = reader.ReadLine()!;
+
+        Console.WriteLine(Hash(str, a, m));
+    }
+
+    private static long Hash(string str, int a, int m) => 
+        str.Aggregate<char, long>(0, (current, c) => (current * a % m + (int) c) % m);
+}
---
+++ Ya.Practicum.SprintFour/CPrefixHashes.cs
@@ -0,0 +1,49 @@
+﻿namespace Ya.Practicum.SprintFour;
+
+public static class CPrefixHashes
+{
+    public static void Execute()
+    {
+        using var reader = new StreamReader(Console.OpenStandardInput());
+        var a = int.Parse(reader.ReadLine()!);
+        var m = int.Parse(reader.ReadLine()!);
+        var s = reader.ReadLine()!;
+        var intervals = int.Parse(reader.ReadLine()!);
+
+        var hashes = GetPrefixHashes(s, a, m);
+        var powers = GetPowers(s, a, m);
+
+        for (var i = 0; i < intervals; i++)
+        {
+            var interval = reader.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(GetHash(hashes, powers, m, interval));
+        }
+    }
+
+    private static double GetHash(long[] hashes, long[] powers, long m, int[] interval)
+    {
+        var start = interval[0];
+        var stop = interval[1];
+
+        return (hashes[stop] + m - (hashes[start] * powers[stop - start]) % m) % m;
+    }
+
+    private static long[] GetPowers(string str, int a, int m)
+    {
+        var powers = new long[str.Length + 1];
+        powers[0] = 1;
+        for (var i = 1; i <= str.Length; i++)
+            powers[i] = powers[i - 1] * a % m;
+
+        return powers;
+    }
+
+    private static long[] GetPrefixHashes(string str, int a, int m)
+    {
+        var hashes = new long[str.Length + 1];
+        for (var i = 1; i <= str.Length; i++)
+            hashes[i] = (hashes[i - 1] * a % m + str[i - 1]) % m;
+
+        return hashes;
+    }
+}
