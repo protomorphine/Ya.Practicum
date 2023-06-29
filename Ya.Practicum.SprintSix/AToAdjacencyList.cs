52e910ddb8271de96df482fb253f0b41a9df921b
Зайцев Даниил Романович <d.zaycev@tanto-s.ru>
2023-06-29 09:15:04 +0300

    start sprint 6

---
+++ Ya.Practicum.SprintSix/AToAdjacencyList.cs
@@ -0,0 +1,45 @@
+﻿namespace Ya.Practicum.SprintSix;
+
+/// <summary>
+/// Алла пошла на стажировку в студию графического дизайна, где ей дали такое задание: для очень большого
+/// числа ориентированных графов преобразовать их список рёбер в список смежности. Чтобы побыстрее решить эту задачу,
+/// она решила автоматизировать процесс.
+///Помогите Алле написать программу, которая по списку рёбер графа будет строить его список смежности.
+/// </summary>
+public static class AToAdjacencyList
+{
+    public static void Execute()
+    {
+        using var reader = new StreamReader(Console.OpenStandardInput());
+        var counts = reader.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+        var (n, m) = (counts[0], counts[1]);
+
+        var vertices = Enumerable.Range(1, n).ToList();
+        var edges = new List<KeyValuePair<int, int>>();
+        var result = new List<KeyValuePair<int, List<int>>>();
+
+        for (var i = 0; i < m; i++)
+        {
+            var read = reader.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+            edges.Add(new KeyValuePair<int, int>(read[0], read[1]));
+        }
+
+        foreach (var vertex in vertices)
+        {
+            if (edges.Exists(it => it.Key == vertex))
+            {
+                result.Add(
+                    new KeyValuePair<int, List<int>>(
+                        edges.Count(it => it.Key == vertex),
+                        edges.Where(it => it.Key == vertex).Select(it => it.Value).ToList()
+                    ));
+                continue;
+            }
+
+            result.Add(new KeyValuePair<int, List<int>>(0, Enumerable.Empty<int>().ToList()));
+        }
+
+        foreach (var pair in result) 
+            Console.WriteLine($"{pair.Key} {string.Join(" ", pair.Value)}");
+    }
+}
---
+++ Ya.Practicum.SprintSix/BEdgesToMatrix.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Ya.Practicum.SprintSix;
+
+/// <summary>
+/// Алла успешно справилась с предыдущим заданием, и теперь ей дали новое. На этот раз список рёбер ориентированного
+/// графа надо переводить в матрицу смежности. Конечно же, Алла попросила вас помочь написать программу для этого.
+/// </summary>
+public static class BEdgesToMatrix
+{
+    public static void Execute()
+    {
+        using var reader = new StreamReader(Console.OpenStandardInput());
+        var counts = reader.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+        var (n, m) = (counts[0], counts[1]);
+        
+        var edges = new List<KeyValuePair<int, int>>();
+        for (var i = 0; i < m; i++)
+        {
+            var read = reader.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+            edges.Add(new KeyValuePair<int, int>(read[0], read[1]));
+        }
+
+        var result = new int[n, n];
+
+        foreach (var edge in edges) 
+            result[edge.Key - 1, edge.Value - 1] = 1;
+
+        for (var i = 0; i < n; i++)
+        {
+            var row = new StringBuilder();
+            for (var j = 0; j < n; j++) 
+                row.Append($"{result[i, j]} ");
+
+            Console.WriteLine(row);
+        }
+    }
+}
