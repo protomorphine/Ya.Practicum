fe9c4cbb893d8f06a307f293a8f33f8168d96bee
Daniil <kupernewfag@gmail.com>
2023-08-13 03:54:01 -0700

    added new task

---
+++ Ya.Practicum.SprintEight/LPrefixFunction.cs
@@ -0,0 +1,26 @@
+﻿namespace Ya.Practicum.SprintEight;
+
+/// <summary>
+/// В этой задаче вам необходимо посчитать префикс-функцию для заданной строки.
+/// </summary>
+public static class LPrefixFunction
+{
+    public static void Execute()
+    {
+        using var reader = new StreamReader(Console.OpenStandardInput());
+        var str = reader.ReadLine()!;
+
+        var prefix = new int[str.Length];
+        for (var i = 1; i < str.Length; i++)
+        {
+            var k = prefix[i - 1];
+
+            while (k != 0 && str[k] != str[i]) 
+                k = prefix[k - 1];
+
+            prefix[i] = str[k] == str[i] ? k + 1 : 0;
+        }
+        
+        Console.WriteLine(string.Join(' ', prefix));
+    }
+}
--- Ya.Practicum.SprintEight/Program.cs
+++ Ya.Practicum.SprintEight/Program.cs
@@ -1,3 +1,3 @@
 ﻿using Ya.Practicum.SprintEight;
 
-GShiftSearch.Execute();
+LPrefixFunction.Execute();
